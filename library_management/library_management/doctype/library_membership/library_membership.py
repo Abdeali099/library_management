cc719db247a8433de8b8aea0dcf76508630f0fca
Abdeali <abdealiking786@gmail.com>
2024-01-11 11:13:38 +0530

    feat : added setting validation in membership

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -4,13 +4,14 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.document import DocStatus
-
+from frappe import utils
 
 class LibraryMembership(Document):
     
-    
-    
     # check before submitting this document
+    
+    # If current membership_form's `from date` > already available `to date``
+    #  Then new membership can't be created because old one is not expire right now 
     
     def before_submit(self):
         
@@ -25,3 +26,11 @@ class LibraryMembership(Document):
   
         if exists:
             frappe.throw("There is an Active membership for this member")
+
+        # get loan period and compute to_date by adding loan period
+        loan_period = frappe.db.get_single_value("Library Setting","loan_period")
+        self.to_date = utils.add_days(self.from_date,loan_period or 30)
+        
+    # FIXME this was not worked 
+    def after_submit(self):
+        frappe.msgprint(msg="Successfully Membership created.",title="Success",indicator="green")
