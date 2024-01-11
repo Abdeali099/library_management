8f8c14e120a01f59fb04129876b003e8306e4c82
Abdeali <abdealiking786@gmail.com>
2024-01-11 12:16:53 +0530

    feat : added maximum limit for one user to issue article

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -4,8 +4,10 @@ from frappe.model.document import Document,DocStatus
 class LibraryTransaction(Document):
 
     def before_submit(self):
+        
         if self.type == 'Issue':
             self.validate_issue()
+            self.validate_maximum_limit()
             # set the article status to be Issued
             article = frappe.get_doc('Article', self.article)
             article.status = 'Issued'
@@ -17,6 +19,8 @@ class LibraryTransaction(Document):
             article = frappe.get_doc('Article', self.article)
             article.status = 'Available'
             article.save()
+            
+        frappe.msgprint(msg=f"Successfully {self.type}ed Book",title="Success",indicator="green")
 
     def validate_issue(self):
         self.validate_membership()
@@ -24,6 +28,18 @@ class LibraryTransaction(Document):
         # article cannot be issued if it is already issued
         if article.status == 'Issued':
             frappe.throw('Article is already issued by another member')
+
+    def validate_maximum_limit(self):
+        max_articles = frappe.db.get_single_value("Library Setting","max_articles")
+
+        count = frappe.db.count("Library Transaction",{
+            "library_member" : self.library_member,
+            "Type" : "Issue",
+            "docstatus" : DocStatus.submitted()
+        })
+        
+        if count >= max_articles:
+            frappe.throw("Maximum Limit reached for issuing articles!!")
 
     def validate_return(self):
         article = frappe.get_doc('Article', self.article)
@@ -33,13 +49,16 @@ class LibraryTransaction(Document):
 
     def validate_membership(self):
         # check if a valid membership exist for this library member
+        
+        # date of return or issue :  form_date < date <= to_date
+        
         valid_membership = frappe.db.exists(
             'Library Membership',
             {
                 'library_member': self.library_member,
                 'docstatus': DocStatus.submitted(),
                 'from_date': ('<', self.date),
-                'to_date': ('>', self.date),
+                'to_date': ('>=', self.date),
             },
         )
         if not valid_membership:
