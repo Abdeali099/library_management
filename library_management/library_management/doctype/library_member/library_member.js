7fe2fd58ec43bf872d2de4eb08f3d9587672df91
Abdeali <abdealiking786@gmail.com>
2024-01-19 11:33:00 +0530

    feat : added custom buttons in library_member doctype.

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, AbdeTion and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
+frappe.ui.form.on("Library Member", {
+	
+    refresh : function (form) {
+        
+        form.add_custom_button('Create Membership',() => {
+            frappe.new_doc("Library Membership",{
+                library_member: form.doc.name
+            })
+        })
 
-// 	},
-// });
+        form.add_custom_button("Create Transaction",()=>{
+            frappe.new_doc("Library Transaction",{
+                library_member:form.doc.name
+            })
+        })
+
+    }
+
+});
